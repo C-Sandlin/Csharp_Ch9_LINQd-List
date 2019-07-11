8779a7dbde00be96f0f132f7b4750274bf93a175
Colin Sandlin <csandlin010@gmail.com>
2019-07-11 14:49:21 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LINQd_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
